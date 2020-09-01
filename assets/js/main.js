b01770640d7bfbba0c23cd4b427b5c2d4752b923
Vladyslav Bondarenko <vladyslav@nevercode.io>
2020-09-01 10:26:19 +0300

    Revert "add menu from main page (#153)"
    This reverts commit b964b6fcb275f9d0326c46cb325f1e7e659fd7db.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,19 @@
-const desktopScreenWidth = 1001;
-
 // Category toggle open
-var navCategory = $('[data-js-docs-menu-item]')
+var navCategory = $('#docs-menu li')
 navCategory.each(function() {
-  $('[data-js-category-name]', this).on('click', function() {
+  $('.category-name', this).on('click', function() {
     var $parent = $(this).parent()
 
-    $parent.find('[data-js-category-posts]').slideToggle(150, function complete() {
+    $parent.find('.category-posts').slideToggle(150, function complete() {
       $parent.toggleClass('open')
     })
   })
 })
 // Menu toggle
-$('[data-js-docs-menu-toggle]').on('click', function() {
-  $('[data-js-docs-menu-toggle-icon]').toggleClass('open')
-  $('[data-js-docs-menu]').toggleClass('open')
+var menuToggle = $('#menu-toggle')
+menuToggle.on('click', function(e) {
+  $(this).toggleClass('open')
+  $('#docs-menu').toggleClass('open')
 })
 
 // Create permalinks
@@ -72,185 +71,30 @@ $(document).ready(function() {
   contentTable.each(function() {
     $(this).wrap('<div class="table-wrap"></div>')
   })
-  elementsTopPosition()
-})
-
-$(document).ready(function() {
-
-  window.userRequest = fetchUser()
-
-  // Fetch user
-  async function fetchUser() {
-      const url = '{{ site.Param "backendURL" }}/user'
-      const timeout = 3000
-
-      const controller = new AbortController()
-      const signal = controller.signal
-
-      setTimeout(() => controller.abort(), timeout)
-
-      try {
-          const response = await fetch(url, {
-              mode: 'cors',
-              credentials: 'include',
-              headers: { Accept: 'application/json' },
-              signal,
-          })
-          try {
-              return { response, json: await response.json() }
-          } catch (error) {
-              return { response, error }
-          }
-      } catch (error) {
-          return { error }
-      }
-  }
-
-  // Menu toggle
-  $('[data-js-header-menu-toggle]').on('click', function() {
-      $(this).toggleClass('open')
-      const visible = $('[data-js-header-menu-wrap]').is(':visible')
-      if (visible) {
-          $('[data-js-header-menu-wrap]').slideUp(200)
-      } else {
-          $('[data-js-header-menu-wrap]').slideDown(200)
-      }
-  })
-
-  // Open extenral links in new tab
-  $('a').each(function() {
-      var hostName = window.location.hostname
-      var href = $(this).attr('href')
-      if (href && !href.includes(hostName) && !href.startsWith('/') && !href.startsWith('#')) {
-          $(this).attr('target', '_blank')
-      }
-  })
-
-  authenticateUser()
-
-  async function authenticateUser() {
-    const { response, json } = await window.userRequest
-    window.auth = { loaded: true }
-
-    if (response && response.ok && json && json.user) {
-        Object.assign(window.auth, json.user.user)
-        if (json.user.ok) {
-            window.loggedIn = true
-            window.auth._id = json.user._id
-            $('[data-js-header-auth-user]').addClass('transition-in')
-            $('[data-js-header-user-avatar]').html('<img src="' + auth.avatarUrl + '" alt=""/>')
-        } else {
-            window.loggedIn = false
-            $('[data-js-header-auth-visitor]').addClass('transition-in')
-        }
-    } else {
-        window.loggedIn = false
-        $('[data-js-header-auth-visitor]').addClass('transition-in')
-    }
-    $('[data-js-header-auth-loading]').addClass('transition-out')
-    setTimeout(function() {
-        $('[data-js-header-auth-loading-grey-line]').hide()
-    }, 1000)
-  }
-
-  $('[data-js-header-auth-logout]').on('click', userLogout)
-
-  async function userLogout() {
-    $('[data-js-header-authentication]').addClass('loading')
-    $('[data-js-header-auth-loading]')
-        .removeClass('transition-out')
-        .addClass('transition-in')
-    $('[data-js-header-auth-loading-grey-line]').show()
-    if ($(window).innerWidth() < 841) {
-        $('[data-js-header-menu-toggle]').removeClass('open')
-        $('[data-js-header-menu-wrap]').slideUp(200)
-    }
-    $('[data-js-header-menu-wrap]').slideUp(200)
-
-    const url = '{{ site.Param "backendURL" }}/logout'
-    const options = {
-        method: 'GET',
-        mode: 'cors',
-        credentials: 'include',
-        headers: { Accept: 'application/json' },
-    }
-    try {
-        await fetch(url, options)
-        $('[data-js-header-auth-user]')
-            .removeClass('transition-in')
-            .addClass('transition-out')
-        setTimeout(function() {
-            $('[data-js-header-auth-loading-grey-line]').hide()
-            $('[data-js-header-auth-visitor]')
-                .removeClass('transition-out')
-                .addClass('transition-in')
-        }, 1000)
-    } catch (error) {
-        location.reload()
-    } finally {
-        auth = { loaded: true }
-        window.loggedIn = false
-        $('[data-js-header-authentication]').removeClass('loading')
-        $('[data-js-header-auth-loading]')
-            .removeClass('transition-in')
-            .addClass('transition-out')
-    }
-  }
-})
-
-let lastScrollPosition = 0
-const sidebar = $('[data-js-sidebar]')
-const header = $('[data-js-header]')
-const docsMenu = $('[data-js-docs-menu]')
-const contentWrap = $('[data-js-content-wrap]')
-const searchResults = $('[data-js-search-results]')
-
-if ($(window).scrollTop() === 0) {
-  header.css('top', 0)
-  sidebar.css('top', header.innerHeight())
-}
-
-$(window).on('scroll', function () {
-  const currentScrollPosition = $(window).scrollTop()
-  // do not consider scrolling bounce effect as scrolling
-  if (currentScrollPosition >= 0 && currentScrollPosition <= $('body').height() - $(window).height()) {
-      window.scrollingDown = currentScrollPosition > lastScrollPosition
-      lastScrollPosition = currentScrollPosition
-  }
-})
-
-$(window).on('load scroll resize', function() {
-  const headerHeight = header.innerHeight()
-  const sidebarHeight = sidebar.innerHeight()
-  if (window.innerWidth < desktopScreenWidth) {
-    if (window.scrollingDown) {
-      sidebar.css('top', 0)
-      header.css('top', -headerHeight)
-      docsMenu.css('top', sidebarHeight)
-      searchResults.css('margin-top', sidebarHeight)
-      contentWrap.css('paddingTop', 0)
-    } else {
-      header.css('top', 0)
-      sidebar.css('top', headerHeight)
-      docsMenu.css('top', headerHeight + sidebarHeight)
-      searchResults.css('margin-top', headerHeight + sidebarHeight)
-      contentWrap.css('paddingTop', headerHeight)
-    }
-  } else {
-    docsMenu.css('top', 90)
-    searchResults.css('margin-top', 76)
-  }
-  elementsTopPosition() // TOC
 })
 
 // Table of content
+
+let lastScrollPosition = 0
+
+$(document).ready(function () {
+    elementsTopPosition()
+})
+$(window).on('load scroll resize', function () {
+    elementsTopPosition()
+})
+$(window).on('scroll', function () {
+    const currentScrollPosition = $(window).scrollTop()
+    window.scrollingDown = currentScrollPosition > lastScrollPosition
+    lastScrollPosition = currentScrollPosition
+})
 
 // Adjust elements positions depending on content shown
 function elementsTopPosition() {
   const windowHeight = $(window).height()
   const topOfWindow = $(window).scrollTop()
   const footerPosition = $('#footer').offset().top
-  const toc = $('[data-js-toc]')
+  const toc = $('#toc')
   const progress = (topOfWindow / (footerPosition - windowHeight)) * 100
 
   if (toc.length) {
@@ -260,7 +104,7 @@ function elementsTopPosition() {
           heightDifference = tableOfContentHeight - windowHeight * 0.8
           contentTablePull = heightDifference * progress / 100
       }
-      const tableOfContentTop = 70 - contentTablePull
+      const tableOfContentTop = 30 - contentTablePull
       toc.css('top', tableOfContentTop)
   }
 }
